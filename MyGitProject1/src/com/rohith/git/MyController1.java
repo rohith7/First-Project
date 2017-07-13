629dc6b192599ad443ac442f0038821f785d4a2e
rohith7 <danush25vip@gmail.com>
2017-07-13 15:52:57 -0600

    # WARNING: head commit changed in the meantime
    Added controller class

---
+++ MyGitProject1/src/com/rohith/git/MyController1.java
@@ -0,0 +1,11 @@
+package com.rohith.git;
+
+public class MyController1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I LOVE YOU BABY");
+	}
+
+}
